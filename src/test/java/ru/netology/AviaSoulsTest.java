3d020a607d0aaff74ed45a7f750ca73e98ddc490
Kirill Brunkov <brunkov.kirill@mail.ru>
2023-04-08 12:26:30 +0700

    first commit

---
+++ src/test/java/ru/netology/AviaSoulsTest.java
@@ -0,0 +1,64 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AviaSoulsTest {
+
+    @Test
+    public void testSortTickets() {
+        AviaSouls aviaSouls = new AviaSouls();
+        Ticket ticket1 = new Ticket("MSK", "SPB", 200, 10, 12);
+        Ticket ticket2 = new Ticket("MSK", "SPB", 100, 11, 14);
+        Ticket ticket3 = new Ticket("MSK", "UFA", 200, 10, 16);
+        Ticket ticket4 = new Ticket("MSK", "SPB", 400, 7, 7);
+        Ticket ticket5 = new Ticket("UFA", "SPB", 800, 4, 9);
+        Ticket ticket6 = new Ticket("MSK", "SPB", 200, 6, 10);
+        Ticket ticket7 = new Ticket("NEW", "LA", 300, 15, 18);
+        Ticket ticket8 = new Ticket("MSK", "SPB", 500, 7, 8);
+        aviaSouls.add(ticket1);
+        aviaSouls.add(ticket2);
+        aviaSouls.add(ticket3);
+        aviaSouls.add(ticket4);
+        aviaSouls.add(ticket5);
+        aviaSouls.add(ticket6);
+        aviaSouls.add(ticket7);
+        aviaSouls.add(ticket8);
+
+        Ticket[] expected = {ticket2, ticket1, ticket6, ticket4, ticket8};
+        Ticket[] actual = aviaSouls.search("MSK", "SPB");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testSortTicketsWithComparator() {
+        AviaSouls aviaSouls = new AviaSouls();
+        Ticket ticket1 = new Ticket("MSK", "SPB", 200, 10, 12); // 2
+        Ticket ticket2 = new Ticket("MSK", "SPB", 100, 11, 14); // 3
+        Ticket ticket3 = new Ticket("MSK", "UFA", 200, 10, 16);
+        Ticket ticket4 = new Ticket("MSK", "SPB", 400, 7, 17); // 10
+        Ticket ticket5 = new Ticket("UFA", "SPB", 800, 4, 9);
+        Ticket ticket6 = new Ticket("MSK", "SPB", 200, 6, 10); // 4
+        Ticket ticket7 = new Ticket("NEW", "LA", 300, 15, 18);
+        Ticket ticket8 = new Ticket("MSK", "SPB", 500, 7, 8); // 1
+        aviaSouls.add(ticket1);
+        aviaSouls.add(ticket2);
+        aviaSouls.add(ticket3);
+        aviaSouls.add(ticket4);
+        aviaSouls.add(ticket5);
+        aviaSouls.add(ticket6);
+        aviaSouls.add(ticket7);
+        aviaSouls.add(ticket8);
+        Comparator<Ticket> comparator = new TicketTimeComparator();
+
+        Ticket[] expected = {ticket8, ticket1, ticket2, ticket6, ticket4};
+        Ticket[] actual = aviaSouls.search("MSK", "SPB", comparator);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
